ecf0c0e3da79e4a1c1a07a9f468af40848225b15
ClassroomAccount <jpepe44@gmail.com>
2019-03-09 15:14:37 -0500

    update imports

---
+++ app/src/main/java/com/course/example/mongodbfirstexample/MainActivity.java
@@ -0,0 +1,49 @@
+package com.course.example.mongodbfirstexample;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+import com.mongodb.MongoClient;
+import com.mongodb.Block;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoCursor;
+import com.mongodb.client.MongoDatabase;
+
+import static com.mongodb.client.model.Filters.*;
+import com.mongodb.client.result.DeleteResult;
+import static com.mongodb.client.model.Updates.*;
+import com.mongodb.client.result.UpdateResult;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import org.bson.Document;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Thread t = new Thread(task);
+        t.start();
+    }
+
+    Runnable task = new Runnable(){
+        public void run(){
+
+            MongoClient mongoClient = new MongoClient("frodo.bentley.edu", 27017);
+            MongoDatabase database = mongoClient.getDatabase("world");
+            MongoCollection<Document> collection = database.getCollection("cities");
+
+            MongoCursor<Document> cursor = collection.find().iterator();
+
+                while (cursor.hasNext()) {
+                    Log.i("Cities", (String) (cursor.next().toJson()));
+                }
+                cursor.close();
+        }
+    };
+}
